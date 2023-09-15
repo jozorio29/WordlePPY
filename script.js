e7206acd013703704f97b839448ee1b69cd31503
jozorio29 <130694329+jozorio29@users.noreply.github.com>
2023-09-14 20:30:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-let intentos =  6;
-let palabra = "APPLE"; 
+let intentos =  5;
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH'];
+
+
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+
 window.addEventListener('load', init)
-const button = document.getElementById("guess-button");
-const input = document.getElementById("guess-input");
-const valor = input.value; 
-button.addEventListener("click", intentar);
 
 function init() {
     console.log('Esto se ejecuta solo cuando se carga la pagina web')
 }
 
+const INPUT = document.getElementById("guess-input");
+const VALOR = INPUT.value; 
+
+const BUTTON = document.getElementById("guess-button");
+BUTTON.addEventListener("click", intentar);
+
+for (let i in palabra) {
+    console.log(palabra[i]);
+}
 function intentar() {
-    const INTENTO = leerIntento();
-   if (INTENTO === palabra) {
-    terminar("<h1>¡GANASTE!😀</h1>")
-    return
-   }
+        const INTENTO = leerIntento();
+        if (INTENTO === palabra) {
+            terminar("<h1>GANASTE!</h1>")
+            return
+        }
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
    for (let i in palabra) {
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
     if (INTENTO[i] === palabra[i]) {
-        console.log(INTENTO[i], "VERDE")
+        SPAN.innerHTML = INTENTO[i];
+        SPAN.style.backgroundColor = '#79b851';
     } else if ( palabra.includes(INTENTO[i])) {
-        console.log(INTENTO[i], "AMARILLO")
+        SPAN.innerHTML = INTENTO[i];
+        SPAN.style.backgroundColor = '#f3c237';
     } else {
-        console.log(INTENTO[i], "GRIS")
+        SPAN.innerHTML = INTENTO[i];
+        SPAN.style.backgroundColor = '#a4aec4';
     }
+    ROW.appendChild(SPAN)
    }
-        intentos--
+   GRID.appendChild(ROW)
+
+        intentos--;
         if (intentos===0) {
-            terminar("<h1>¡PERDISTE!😖</h1>")
+            terminar("<h1>PERDISTE!</h1>")
         }
 }
 
@@ -37,4 +57,13 @@ function leerIntento() {
     intento = intento.toUpperCase();
     return intento;
 }
+
+function terminar(mensaje) {
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    BUTTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
 
